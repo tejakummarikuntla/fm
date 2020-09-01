b6712c9dc8d17dc90d7f6dc2c7a103ab83b3cd39
github_chaitanyamuvvala <muvvalachaitanya05@gmail.com>
2020-09-01 17:52:54 +0530

    Add: Update Image

--- src/components/sections/Advisory.js
+++ src/components/sections/Advisory.js
@@ -18,7 +18,7 @@ const Card = styled.div((props) => [
 ]);
 const Image = styled.div((props) => [
   `background-image: url("${props.imageSrc}");`,
-  tw`rounded md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-80 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`,
+  tw`rounded md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-128 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`,
 ]);
 const Details = tw.div`mt-4 md:mt-0 md:max-w-md mx-4 sm:mx-8 md:mx-4 lg:mx-8`;
 const Subtitle = tw.div`font-bold tracking-wide text-secondary-100`;
